d7f6798b7c089f745a79215faf06282033eb336e
unknown <arindasherinah1@gmail.com>
2024-10-03 11:08:20 +0300

    initial

---
+++ control_flow_structures.py
@@ -0,0 +1,21 @@
+#control flow structures
+#it determines the order in which the program is to be excuted basing on loops and conditions
+#conditional statements are utilised by if, elif, else
+# create a program that asks a user for the food type bought, the program should print you bought chicken if the user enters chicken, liver if the user enters liver, else fish if the user enters fish
+
+food_type = input("Enter the food_type bought:")
+if food_type == 'chicken':
+    print(f"You bought chicken from the market")
+elif food_type == 'liver':
+    print(f"You bought liver from the market")
+else:
+    food_type == 'fish': 
+    print(f"You bought fish from the market")
+
+else:
+    #Logical operators
+    
+
+
+
+    
